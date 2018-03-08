16815d9ae7ccb225e7fb5c4681d2eb5048f93703
Andrew Price <aprice2704@gmail.com>
2018-03-07 17:15:01 -0800

    Scratchings

--- seltest.go
+++ seltest.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	tree "github.com/aprice2704/seldon/tree"
 	wbs "github.com/aprice2704/seldon/wbs"
 )
 
 func main() {
 
 	test := wbs.Piece{}
+	ttree := tree.TreeNode{}
 
 }
--- wbs/wbs.go
+++ wbs/wbs.go
@@ -1,9 +1,10 @@
 package wbs
 
+import "github.com/aprice2704/seldon/tree"
+
 type Piece struct {
-	Name     string
-	Serial   int
-	ID       string
-	Parent   *Piece
-	Children []*Piece
+	Name   string
+	Serial int
+	ID     string
+	tree.TreeNode
 }
